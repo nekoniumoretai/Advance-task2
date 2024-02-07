f369d69c828e287d4239f9f4d18da23116259a07
nekoniumoretai <152590285+nekoniumoretai@users.noreply.github.com>
2024-02-07 07:00:40 +0000

    Fix 検索機能

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -23,7 +23,7 @@ class SearchesController < ApplicationController
     else
       # 上記の条件に当てはまらない場合の条件分岐 / book の検索を行います
 
-      @records = Books.search_for(@content, @method)
+      @records = Book.search_for(@content, @method)
       # 選択された検索対象（書籍（book））に対して、 search_forメソッド を呼び出して検索を実行し、結果を @records変数 に代入する
 
     end
