efa4d36362c68dab6c2cc9454fe3ba429a26e7c8
nekoniumoretai <152590285+nekoniumoretai@users.noreply.github.com>
2024-02-09 08:52:12 +0000

    update いいね、コメント機能の非同期化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,11 +2,12 @@ class FavoritesController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: @book.id)
-    # favorites = その時ログインしているユーザーの
-    favorite.save
+    @favorite = current_user.favorites.new(book_id: @book.id)
+    # favorite = その時ログインしているユーザーの
+    @favorite.save
     # いいねを保存
     # redirect_back(fallback_location: root_path)
+    render 'replace_btn'
   end
 
   def destroy
@@ -14,5 +15,6 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
     # redirect_back(fallback_location: root_path)
+    render 'replace_btn'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
+
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
 
   resources :books, only: [:index,:show,:edit,:create,:update, :destroy] do
-    resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
 
   resources :users, only: [:index,:show,:edit,:update] do
